14fa35856bd4a6de31ea120289d0bc5e50639a58
bhoomiy <bhoomiyadav039@gmail.com>
2026-06-12 13:59:28 +0530

    Add solution for Binary Search

--- LeetCode/Binary-Search/Solution.java
+++ LeetCode/Binary-Search/Solution.java
@@ -1,7 +1,7 @@
 1class Solution {
 2    public int search(int[] nums, int target) {
 3        int low = 0 , high = nums.length -1;
-4        for(int i=0;i<nums.length ; i++){
+4        while(low<= high){
 5            int mid = (high + low)/ 2;
 6            if(nums[mid] == target) return mid;
 7            else if(nums[mid]> target) high = mid - 1;
